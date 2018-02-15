288f9a3a821ea1a920e3f038a44c7599aab7fd78
ism41670634 <ism41670634@a11.informatica.escoladeltreball.org>
2018-02-15 12:47:55 +0100

    first commit

---
+++ buclefor1.py
@@ -0,0 +1,12 @@
+#coding: utf-8
+x = input("Pide un numero")
+y = input("Pide otro numero")
+while (y<x):
+	print "es menor que x"
+	y = input("Vuelve a pedir otro numero")
+for i in (x,y):
+if i % 2 == 0
+	print "Par"
+else:
+	print "impar"
+
---
+++ buclefor2.py
@@ -0,0 +1,5 @@
+#coding: utf-8
+print "DIVISORES"
+x = input("Indique un numero mayor que zero:")
+
+
